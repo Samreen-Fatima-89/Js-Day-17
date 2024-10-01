813758792203ccbc39e070237f31e74ec15c2205
Samreen Fatima <irfankhans786787789@gmail.com>
2024-10-01 21:10:10 +0500

    completed!

---
+++ app.js
@@ -0,0 +1,28 @@
+// function abc() {
+//   var text = document.getElementById("text");
+//   var fullText =
+//     "Lorem ipsumdolor sit amet consectetur adipisicing elit. Eius non cumque  t perferendis itaque odit consequatur, vero eos quae officiis sapiente eaque tempore asperiores voluptatum natus beatae. Facere, aut corrupti?";
+//   var lessText = "Lorem ipsumdolor sit amet consectetur adipisicing elit.";
+
+//   if (btn.innerText === "See More") {
+//     text.innerText = fullText;
+//     btn.innerText = "See Less";
+//   } else if (btn.innerText === "See Less") {
+//     text.innerText = lessText;
+//     btn.innerText = "See More";
+//   }
+// }
+
+// var userName = document.getElementById("userName");
+
+// function userData() {
+//   var userEmail = document.getElementsByTagName("h1");
+
+//   console.log(userEmail);
+// }
+//   if (!userName.value) {
+//     alert("Please enter yout name");
+//   } else {
+//     alert(userName.value);
+//   }
+//   userName.value ? alert("Please enter your name!") : alert(userName.value);
